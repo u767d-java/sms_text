c102d262e54e9aaaf0c07afd17d48263003a959d
offcn-java <1500651002@qq.com>
2020-10-27 21:57:15 +0800

    Initial commit

---
+++ src/main/java/com/offcn/sms/SmsTest.java
@@ -0,0 +1,47 @@
+package com.offcn.sms;
+
+
+
+
+import org.apache.http.HttpResponse;
+
+import java.util.HashMap;
+import java.util.Map;
+
+
+public class SmsTest {
+    public static void main(String[] args) {
+        String host = "http://dingxin.market.alicloudapi.com";
+        String path = "/dx/sendSms";
+        String method = "POST";
+        String appcode = "24be8e5ba1b243d484192c628a63a9c7";
+        Map<String, String> headers = new HashMap<String, String>();
+        //最后在header中的格式(中间是英文空格)为Authorization:APPCODE 83359fd73fe94948385f570e3c139105
+        headers.put("Authorization", "APPCODE " + appcode);
+        Map<String, String> querys = new HashMap<String, String>();
+        querys.put("mobile", "13546717573");
+        querys.put("param", "code:231148193");
+        querys.put("tpl_id", "TP1711063");
+        Map<String, String> bodys = new HashMap<String, String>();
+
+
+        try {
+            /**
+             * 重要提示如下:
+             * HttpUtils请从
+             * https://github.com/aliyun/api-gateway-demo-sign-java/blob/master/src/main/java/com/aliyun/api/gateway/demo/util/HttpUtils.java
+             * 下载
+             *
+             * 相应的依赖请参照
+             * https://github.com/aliyun/api-gateway-demo-sign-java/blob/master/pom.xml
+             */
+            HttpResponse response = HttpUtils.doPost(host, path, method, headers, querys, bodys);
+            
+            System.out.println(response.toString());
+            //获取response的body
+            //System.out.println(EntityUtils.toString(response.getEntity()));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
